5b90f0a5efd0cd7646c23345b624efb18ffcc36a
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-12-09 10:54:55 +0530

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
@@ -36,7 +37,14 @@ import { ApiInterceptor } from './interceptor/http-interceptor';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'product', component: ProductComponent },
+      { path: 'categories', component: CategoriesComponent },
+      { path: 'customer', component: CustomerComponent },
+      { path: 'todo', component: TodoComponent },
+      { path: '', redirectTo: 'product' , pathMatch : 'full' },
+    ])
   ],
   providers: [{ provide: ProductService, useClass: ProductnewService },
     CategoriesService,
